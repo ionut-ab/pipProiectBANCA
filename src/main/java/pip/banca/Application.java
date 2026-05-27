db4929be0834c7403c1a893c60a5e2c60699d6b6
octaviandarcaci <octavian.darcaci@student.tuiasi.ro>
2026-05-27 22:58:52 +0300

    Validare conexiune intre admin si user din keycloak

--- src/main/java/pip/banca/Application.java
+++ src/main/java/pip/banca/Application.java
@@ -1,14 +1,7 @@
 package pip.banca;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import pip.banca.entities.User;
-import pip.banca.repositories.UserRepository;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
@@ -17,17 +10,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	CommandLineRunner init(UserRepository userRepository) {
-		return args -> {
-			User user = new User("Alice", "Popescu", "alice@example.com", "07namcartela", "sub podu ros", "nam");
-			try {
-				userRepository.save(user);
-				System.out.println("User saved!");
-			}catch(Exception ex){
-				System.out.println(ex.getMessage());
-			}
-
-		};
-	}
 }
